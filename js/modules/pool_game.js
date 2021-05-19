dcd82c50150187c17b11d081ce41d2f6df002804
Indraraj Karki <endraraaz@gmail.com>
2021-05-19 13:00:56 +0545

    Add game engine

---
+++ js/modules/pool_game.js
@@ -0,0 +1,28 @@
+class PoolGame {
+
+    init = () => {
+        
+        this.gameEnvironment = new GameEnvironment();
+    };
+
+    start = () => {
+
+        this.init();
+
+        this.gameEngine();
+
+    };
+
+    gameEngine = () => {
+
+        canvas.clear();
+        poolGame.gameEnvironment.update();
+        poolGame.gameEnvironment.draw();
+
+        mouse.reset();
+
+        requestAnimationFrame(poolGame.gameEngine);
+    };
+};
+
+var poolGame = new PoolGame();
